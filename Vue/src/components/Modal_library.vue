46fe4816219bb0e297a8e934e53e8494d4558e36
4ndreees <4mrvro@gmail.com>
2024-11-01 12:50:37 -0400

    Creación del Modal

---
+++ Vue/src/components/Modal_library.vue
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<template>
+    <div class="container-info-item">
+
+    </div>
+</template>
+
+<style>
+    .container-info-item {
+        border: 1px solid gray;
+        border-radius: 10px;
+    }
+</style>
