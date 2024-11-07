fb460fb732f8baefe2e72d7c880369d887e6ceb2
4ndreees <4mrvro@gmail.com>
2024-11-07 10:18:32 -0400

    Service y Store de login

---
+++ src/services/login_service/loginService.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+    baseURL: 'http://localhost:3000/users', 
+    withCredentials: false,
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+    }
+});
+
+export default {
+    // Método para login
+    login(email_user, password_user) {
+        return apiClient.post('/login', {
+            email_user,
+            password_user
+        });
+    }
+};
